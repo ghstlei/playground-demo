421aba96225efd329d02130918734fa37b746af6
cih <139817646+ghstlei@users.noreply.github.com>
2023-11-20 15:52:43 -0300

    Add files via upload

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientComponent } from './modules/client/client.component';
 import { EmployeeComponent } from './modules/employee/employee.component';
-import { EmployeeLeaveComponent } from './modules/employee-leave/employee-leave.component';
 import { JobPositionComponent } from './modules/job-position/job-position.component';
+import { EmployeeLeaveComponent } from './modules/employee-leave/employee-leave.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'client' },
+  { path: '', redirectTo: 'client', pathMatch: 'full' },
   { path: 'client', component: ClientComponent},
   { path: 'employee', component: EmployeeComponent},
-  { path: 'employee-leave', component: EmployeeLeaveComponent},
   { path: 'job-position', component: JobPositionComponent},
+  { path: 'employee-leave', component: EmployeeLeaveComponent},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app.module.ts
+++ app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule  } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddComponent } from './modules/product/components/add/add.component';
-import { EditComponent } from './modules/product/components/edit/edit.component';
-import { GridComponent } from './modules/product/components/grid/grid.component';
-import { ProductComponent } from './modules/product/product.component';
+import { AddComponent } from './modules/client/components/add/add.component';
+import { EditComponent } from './modules/client/components/edit/edit.component';
+import { GridComponent } from './modules/client/components/grid/grid.component';
 import { ClientComponent } from './modules/client/client.component';
+import { HeaderComponent } from './header/header/header.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
 import { EmployeeComponent } from './modules/employee/employee.component';
 import { JobPositionComponent } from './modules/job-position/job-position.component';
 import { EmployeeLeaveComponent } from './modules/employee-leave/employee-leave.component';
-
 
 @NgModule({
   declarations: [
@@ -19,15 +21,19 @@ import { EmployeeLeaveComponent } from './modules/employee-leave/employee-leave.
     AddComponent,
     EditComponent,
     GridComponent,
-    ProductComponent,
     ClientComponent,
     EmployeeComponent,
     JobPositionComponent,
-    EmployeeLeaveComponent
+    EmployeeLeaveComponent,
+    HeaderComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
